65aa9c2bd94fa4510b80d62344ab252310308a9f
25-Naufal <naufal_rafif_29rpl@student.smktelkom-mlg.sch.id>
2021-03-10 19:14:40 +0700

    hore berhasil

---
+++ src/parent/Parent.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parent;
+
+/**
+ *
+ * @author ASUS X450JN
+ */
+public class Parent {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        anak object = new anak ();
+        object.tampil();
+    }
+    
+    public Parent(){
+        System.out.println("Hai");
+    }
+    void Man(){
+        System.out.println("Halo");
+    }
+    
+}
---
+++ src/parent/anak.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parent;
+
+/**
+ *
+ * @author ASUS X450JN
+ */
+public class anak extends Parent {
+    
+    public anak(){
+        super();
+        System.out.println("Hello");
+    }
+    
+    public void tampil(){
+        super.Man();
+    }
+    
+}
